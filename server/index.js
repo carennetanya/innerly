36e98ce7456f392b8140317d727e93465317bdbd
carennetanya <netanya.caren@gmail.com>
2026-05-11 20:55:26 +0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ["https://innerly-psi.vercel.app", "http://localhost:5173"]
+  origin: [
+    "https://innerly-psi.vercel.app",
+    "http://localhost:5173",
+    
+  ],
 }));
 app.use(express.json());
 
